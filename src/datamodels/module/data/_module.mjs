057c7ed5a56adb750d4a21c7a8c17bcd19afa827
Bartoszeq-tech <andrzejak.bartosz@gmail.com>
2025-08-16 01:32:52 +0200

    Removed useless folder

--- src/datamodels/module/data/_module.mjs
+++
@@ -1,9 +0,0 @@
-// Export Actors
-export { default as KonosubaActorBase } from "./base-actor.mjs";
-export { default as KonosubaCharacter } from "./actor-character.mjs";
-export { default as KonosubaNPC } from "./actor-npc.mjs";
-
-// Export Items
-export { default as KonosubaItemBase } from "./base-item.mjs";
-export { default as KonosubaItem } from "./item-item.mjs";
-export { default as KonosubaSkill } from "./item-skill.mjs";
